2acd92b23865d1e41f7a3c6c547832c82c0224a1
Ron Shabi <ron@ronsh.net>
2023-05-03 00:10:08 +0300

    added: safer argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <csignal>
+#include <vector>
+#include <cctype>
 
 #include "timer.hpp"
 #include "logging.hpp"
@@ -8,8 +10,9 @@
 void print_usage()
 {
     std::cout << "C++ Pomodoro utility, 2023(c) Ron Shabi.\n";
-    std::cout << "usage: ./pomodoro [duration] [start-offset] --csv\n";
+    std::cout << "usage: pomodoro [duration] [start-offset] --csv\n";
     std::cout << "By default, logs are saved in ~/.pomodoro\n";
+    std::exit(0);
 }
 
 int main(int argc, const char** const argv)
@@ -17,24 +20,43 @@ int main(int argc, const char** const argv)
     usize duration{25*60};
     usize start{0};
 
-    if (argc==1) {
-
+    // Convert argv to string vector 
+    std::vector<std::string> arguments{};
+    for (int i = 1; i < argc; i++) {
+        arguments.push_back(argv[1]);
     }
-    else if (argc==2) {
-        if (std::strcmp(argv[1], "csv")==0) {
-            logging::convert_log_to_csv(logging::get_homedir() + "/.pomodoro/log-"+datetime::date()+".txt");
-            std::exit(0);
+
+    // Argument parsing
+    if (arguments.size() == 1) {
+        int session_duration = std::atoi(arguments.at(0).c_str());
+
+        // not a number
+        if (session_duration <= 0) {
+            // single argument checking is done here
+            if (arguments.at(0) == "csv") {
+                logging::convert_log_to_csv(logging::get_homedir() + "/.pomodoro/log-"+datetime::date()+".txt");
+                std::exit(0);
+            } else {
+                print_usage();
+            }
         }
 
-        duration = std::stoi(argv[1])*60;
-    }
-    else if (argc==3) {
-        duration = std::stoi(argv[1])*60;
-        start = std::stoi(argv[2])*60;
-    }
-    else {
+        // valid number
+        duration = static_cast<usize>(session_duration) * 60;
+    } else if (arguments.size() == 2) {
+        int session_duration =  std::atoi(arguments.at(0).c_str());
+        int starting_offset =  std::atoi(arguments.at(1).c_str());
+
+        // Invalid duration arguments
+        if (session_duration <= 0 || starting_offset <= 0) {
+            print_usage();
+        }
+
+        // Valid duration arguments
+        duration = static_cast<usize>(session_duration) * 60;
+        start = static_cast<usize>(session_duration) * 60;
+    } else {
         print_usage();
-        std::exit(0);
     }
 
     Timer t{start, duration};
